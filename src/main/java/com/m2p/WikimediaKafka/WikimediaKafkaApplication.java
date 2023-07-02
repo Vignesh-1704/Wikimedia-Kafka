7ca0ac2f6e151d3fa8d0ab6b29a73db1fd1bd05b
Vignesh-1704 <vigneshmanikavasagam17@gmail.com>
2023-07-02 13:41:57 +0530

    Add. Created a Multi Module Maven Project with POM as Packaging

---
+++ src/main/java/com/m2p/WikimediaKafka/WikimediaKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.m2p.WikimediaKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaKafkaApplication.class, args);
+	}
+
+}
